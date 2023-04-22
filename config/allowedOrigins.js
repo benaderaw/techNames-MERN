7b860f1d34ad83d2de62e008efa0834d1958f354
benaderaw <128280449+benaderaw@users.noreply.github.com>
2023-04-21 21:51:13 -0400

    Delete config directory

--- config/allowedOrigins.js
+++
@@ -1,9 +0,0 @@
-const { all } = require("../routes/root");
-
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://www.benyamrepairshop.com",
-  "https://www.benyamrepair.com",
-];
-
-module.exports = allowedOrigins;
